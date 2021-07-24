e402574c3628a54e65e3abb2b06f6c056e5c38ec
tiensu <tiensunguyen2103@gmail.com>
2021-07-24 10:57:52 +0700

    Add missing data imputation

---
+++ 02_Data_Cleaning/Missing_Data/Statistical_Imputation/05_predict.py
@@ -0,0 +1,29 @@
+# constant imputation strategy and prediction for the horse colic dataset
+from numpy import nan
+import joblib
+from pandas import read_csv
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+# load dataset
+dataframe = read_csv('horse-colic.csv', header=None, na_values='?')
+# split into input and output elements
+data = dataframe.values
+ix = [i for i in range(data.shape[1]) if i != 23]
+X, y = data[:, ix], data[:, 23]
+# create the modeling pipeline
+pipeline = Pipeline(steps=[('i', SimpleImputer(strategy='constant')), ('m', RandomForestClassifier())])
+# fit the model
+pipeline.fit(X, y)
+
+# save pipeline as model file
+joblib.dump(pipeline, 'model.mod') 
+# load model from file
+model = joblib.load('model.mod')
+
+# define new data
+row = [2, 1, 530101, 38.50, 66, 28, 3, 3, nan, 2, 5, 4, 4, nan, nan, nan, 3, 5, 45.00, 8.40, nan, nan, 2, 11300, 00000, 00000, 2]
+# make a prediction
+yhat = model.predict([row])
+# summarize prediction
+print('Predicted Class: %d' % yhat[0])
